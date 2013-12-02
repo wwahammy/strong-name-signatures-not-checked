7a3079f8d6a9dd5abe2275df90465fd1ff514b51
Eric Schultz <wwahammy@gmail.com>
2013-12-02 16:40:40 -0600

    Initial commit of code

---
+++ StrongNameVerification/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test;
+
+namespace StrongNameVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.WriteLine(class1.TestReturn());
+        }
+    }
+}
